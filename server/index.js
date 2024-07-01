e46a64854740aa08c38e31f23804b053e10d8a55
Arvind-K1 <arvindkandpal2002@gmail.com>
2024-07-01 23:27:34 +0530

    setup basic file structure and config mongodb database

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import { configDotenv } from "dotenv";
+import connectDB from "./config/dbConnection.js";
+import { app } from "./app.js"
+
+configDotenv({
+    path: "./.env"
+})
+
+connectDB()
+    .then(() => {
+        // Also we include app.on for error listen.
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
